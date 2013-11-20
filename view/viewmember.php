6d6f69d8f8447a7963376928da4912a1a73c2194
Robert <robert.lopes@gmail.com>
2013-11-19 23:03:29 -0500

    viewmember prettied up for deployment

--- view/viewmember.php
+++ view/viewmember.php
@@ -1,15 +1,25 @@
 <?php
 	foreach($model as $tree)
 	{
-		echo $tree->name."<br/>";
+		echo "<hr/>";
+		echo "<h3>".$tree->name."</h3>";
 		foreach($tree->branches as $branch)
 		{
-			echo $branch->name."<br/>";
+			echo "<h4>".$branch->name."</h4>";
 			foreach($branch->leaves as $leaf)
 			{
-				echo $leaf->name." ".$leaf->have."<br/>";
+				echo $leaf->name." ";
+			if($leaf->have==0)
+			{
+				echo "<span style='color:red;'><b>I can't find it!!</b></span><br/>";
+			}
+			else
+			{
+				//item 501, mana vehicle turret seems to show up regardless
+				//Must be logically a skill to unlock the usage of this existing item.
+				echo "<span style='color:green;'><b>You got it!</b></span><br/>";
+			}
 			}
 		}
-		echo "<br/><br/>";
 	}
 ?>
