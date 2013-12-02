b35746431d227128dbdf2cb852532eee8f7359bd
Robert <robert.lopes@gmail.com>
2013-12-01 22:39:52 -0500

    Cleaned database table, started InputForest
    Items table had a lot of junk data, improved the import from SOE API to
    pull fewer junk items (like decals, cammo paint, etc) some junk still
    came through may have to clean more.
    
    http://census.soe.com/get/ps2:v2/item?c:limit=9000&c:has=item_category_id

---
+++ model/InputForest.php
@@ -0,0 +1,94 @@
+<?php
+
+	include_once("model/Tree.php");
+	include_once("model/Player.php");
+	Class Forest
+	{
+		public $vehicles;
+		public $suitUpgrades;
+		public $weapons;
+		
+		public function __construct()  
+		{  
+			$this->vehicles = array();
+			$this->suits = array();
+			$this->weapons = array();
+			$this->populate(); //Eventually this will take an array of Certification Trees, for now it will return all
+		}
+		//REDO weaponattachment, looking for item_category_id existing
+		protected function populate() //This whole function will need to be reworked when I start specifying cert trees.
+		{
+			$this->vehicles = $this->popVehicles();
+			$this->suits = $this->popsuits();
+			$this->weapons = $this->popWeapons();
+			
+			$weaponBases = '
+			SELECT b.group_name, p.name, cb.item_id, pa.name AS base_name
+			FROM groups b
+			JOIN group_items cb ON cb.group_id = b.group_id
+			JOIN items p ON p.item_id = cb.item_id
+			LEFT JOIN wattachments a ON cb.item_id = a.attachment_item_id
+			LEFT JOIN items pa ON pa.item_id = a.item_id
+			WHERE cb.item_tag = "W"
+			ORDER BY p.name ASC 
+			';
+			$weaponAttachments = '
+			SELECT p.name, p.item_id, pa.name AS base_name
+			FROM items p
+			JOIN wattachments a ON p.item_id = a.attachment_item_id
+			JOIN items pa ON pa.item_id = a.item_id
+			ORDER BY p.item_id ASC
+			';
+			$suitUpgrades = '
+			SELECT b.group_name, p.name, cb.item_id, pa.name AS base_name
+			FROM groups b
+			JOIN group_items cb ON cb.group_id = b.group_id
+			JOIN items p ON p.item_id = cb.item_id
+			LEFT JOIN item_profile a ON cb.item_id = a.item_id
+			LEFT JOIN profile pa ON pa.profile_id = a.profile_id
+			WHERE cb.item_tag =  "S"
+			ORDER BY p.name ASC 
+			';
+			$vehicle = '
+			SELECT b.group_name, p.name, cb.item_id, a.description AS base_name
+			FROM groups b
+			JOIN group_items cb ON cb.group_id = b.group_id
+			JOIN items p ON p.item_id = cb.item_id
+			LEFT JOIN vehicle_attachment a ON cb.item_id = a.item_id
+			WHERE cb.item_tag =  "V"
+			ORDER BY p.name ASC
+			';
+			$this->runQ($vehicle);
+			$this->runQ($weapons);
+			$this->runQ($suitUpgrades);
+			
+		}
+		protected function runQ($query)
+		{
+			include("model/dbconnect.php");
+			$queryItems = $dbh->prepare($query);
+			$queryItems->execute();
+			$rows = $queryItems->fetchAll();
+			foreach ($rows as $row) 
+			{
+				$treeName = $row["group_name"];
+				if ($row["base_name"]===NULL)
+				{
+					$branchName = $row["name"];
+				}
+				else
+				{
+					$branchName = $row["base_name"];
+				}
+				$eqName = $row["name"];
+				$haveEq = $this->player->getItemByID($row["item_id"]);
+				
+				if (!isset($this->trees[$treeName]))
+				{
+					$this->trees[$treeName] = new Tree($treeName);
+				}
+				$this->trees[$treeName]->addBranch($branchName, $eqName, $haveEq);
+			}
+		}
+	}
+?>
